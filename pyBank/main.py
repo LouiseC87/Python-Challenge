8b256f374f6aa89a2d3932d1f26b7422562709c9
louise cachuela <louie2521@gmail.com>
2020-10-24 21:22:23 -0700

    python Bank

---
+++ pyBank/main.py
@@ -0,0 +1,88 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Variables
+total_months = 0
+net_amount = 0
+monthly_change = []
+month_count = []
+greatest_increase = 0
+greatest_increase_month = 0
+greatest_decrease = 0
+greatest_decrease_month = 0
+average_change = 0
+
+#creating lists
+profits = []
+net_change = []
+months = []
+highest = []
+lowest = []
+
+# Set Path For File
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Open & Read CSV File
+with open(csvpath, newline='') as csvfile:
+    
+    # CSV Reader Specifies Delimiter & Variable That Holds Contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    # Read The Header Row First (Skip This Step If There Is No Header)
+    csv_header = next(csvreader)
+    row = next(csvreader)
+    
+    # Calculate Total Number Of Months, Net Amount Of "Profit/Losses" & Set Variables For Rows
+    previous_row = int(row[1])
+    total_months += 1
+    net_amount += int(row[1])
+    greatest_increase = int(row[1])
+    greatest_increase_month = row[0]
+    
+    # Read Each Row Of Data After The Header
+    for row in csvreader:
+        
+        # Calculate Total Number Of Months Included In Dataset
+        total_months += 1
+        # Calculate Net Amount Of "Profit/Losses" Over The Entire Period
+        net_amount += int(row[1])
+
+        # Calculate Change From Current Month To Previous Month
+        revenue_change = int(row[1]) - previous_row
+        monthly_change.append(revenue_change)
+        previous_row = int(row[1])
+        month_count.append(row[0])
+        
+        # Calculate The Greatest Increase
+        if int(row[1]) > greatest_increase:
+            greatest_increase = int(row[1])
+            greatest_increase_month = row[0]
+            
+        # Calculate The Greatest Decrease
+        if int(row[1]) < greatest_decrease:
+            greatest_decrease = int(row[1])
+            greatest_decrease_month = row[0]  
+        
+    # Calculate The Average & The Date
+    average_change = sum(monthly_change)/ len(monthly_change)
+    
+    highest = max(monthly_change)
+    lowest = min(monthly_change)
+
+        
+
+# Print Analysis
+print(f"Financial Analysis")
+print(f"---------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_amount}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})")
+print(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})")
+
+
+
+
+
